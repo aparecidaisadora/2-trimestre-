8ee6199ed52304449e696d325972dfa44a55cfc1
Isadora Aparecida leite <105376440+aparecidaisadora@users.noreply.github.com>
2024-08-14 09:47:15 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,50 +6,50 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "vocẽ trancaria uma pessoa em uma torre?",
+        enunciado: "você trancaria uma pessoa em uma torre?",
         alternativas: [
             {
                 texto: "sim",
-                afirmacao: "nossa você é maldozo "
+                afirmacao: "você é muito má "
             },
             {
                 texto: "não",
-                afirmacao: "htryyik"
+                afirmacao: "você é sentimental"
             }
         ]
     },
     {
-        enunciado: "você tem cabelo grande",
+        enunciado: "você tem cabelo grande?",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
+                texto: "sim",
+                afirmacao: "wwwbwh."
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+                texto: "não.",
                 afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
             }
         ]
     },
     {
-        enunciado: "você se apaixonaria no p",
+        enunciado: "você gosta de cores escuras?",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "sim.",
+                afirmacao: "e estilosa."
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+                texto: "não, odeio.",
+                afirmacao: "e doce."
             }
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "voê se sacrificaria por alguem que vocẽ ama?",
         alternativas: [
             {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+                texto: "sim.",
+                afirmacao: "não."
             },
             {
                 texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
@@ -58,11 +58,11 @@ const perguntas = [
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: "ja se apaixonou de primera vista? ",
         alternativas: [
             {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+                texto: "sim, sempre.",
+                afirmacao: "eu nunca."
             },
             {
                 texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
